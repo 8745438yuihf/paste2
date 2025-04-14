ee59673eadc194d275e8ab09b0e2a6ac28c3684d
8745438yuihf <emmanuel.2109803@nv.ccsd.net>
2025-04-14 11:53:33 -0700

    Update paste2.lua

--- paste2.lua
+++ paste2.lua
@@ -1,8 +1,26 @@
-local ran = {
-"Head",
-"Torso"
+local toggles = {
+    force_head = true,
+    fake_crouch = true,
+    force_ground = true
 }
-while true do 
-print(ran[math.random(#ran)])
-wait(1)
-end
+
+local old_force = hookfunction(getrenv()._G.modules.Character.IsGrounded, function(...)
+    if toggles.force_ground then
+        return true
+    end
+    return old_force(...)
+end)
+
+    local old_hooks = hookfunction(game.Players.LocalPlayer:FindFirstChild("RemoteEvent").FireServer, function(self, ...)
+        local args = {...}
+
+        if args[1] == 10 and typeof(args[4]) == "Vector3" and toggles.force_head then
+            args[6] = "Head"
+        end
+
+        if args[1] == 2  and toggles.force_crouch then
+            args[2] = true
+        end
+
+        return old_hooks(self, unpack(args))
+    end)
